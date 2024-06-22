845362d7df90606deb9e6482dcfd8590180d65bd
Hardik Halani <hardik.halani14@gmail.com>
2024-06-22 17:29:43 +0530

    Initial Commit

---
+++ TaskManagementApp/TaskManagementAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagementAppApp.swift
+//  TaskManagementApp
+//
+//  Created by Hardik Halani on 22/06/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagementAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
